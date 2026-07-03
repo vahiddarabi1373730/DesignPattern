442e5a54c67b9999a7cfc26e341c66c94f4a7ad3
vahiddarabi1373730 <db.vahid1737@gmail.com>
2026-07-03 15:14:39 +0330

    factory method

--- Starter/Program.cs
+++ Starter/Program.cs
@@ -4,8 +4,63 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hiii");
+            var emailCreator = new EmailCreator();
+            emailCreator.SendNotification("Sending Email");
+
+            var smsCreator = new SMSCreator();
+            smsCreator.SendNotification("Sending SMS");
+
         }
     }
 }
 
+
+
+public interface INotification
+{
+    public void SendMessage(string message);
+}
+
+public class SendEmail : INotification
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+public class SendSms : INotification
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+//FactoryMethod
+public abstract class NotificationCreator
+{
+    public abstract INotification CreateNotification();
+
+    public void SendNotification(string message)
+    {
+        var notification=CreateNotification();
+        notification.SendMessage(message);
+    }
+}
+
+public class EmailCreator : NotificationCreator
+{
+    public override INotification CreateNotification()
+    {
+        return new SendEmail();
+    }
+}
+
+public class SMSCreator : NotificationCreator
+{
+    public override INotification CreateNotification()
+    {
+        return new SendSms();
+    }
+}
